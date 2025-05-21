8a18615ffec14c706b3c36a9d7dddd785b3026a9
Bhaumik Raja <webdevbhaumik@gmail.com>
2025-05-21 18:21:32 -0400

    Initial setup

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+// Setting initial conditions
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
